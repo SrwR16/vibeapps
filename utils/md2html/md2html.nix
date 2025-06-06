ca0edbe6b3486387c7f69ce9eb99c71cc2321bc4
MD. Sarwar Uddin <99182873+SrwR16@users.noreply.github.com>
2025-06-06 23:38:14 +0600

    Update md2html.nix

--- utils/md2html/md2html.nix
+++ utils/md2html/md2html.nix
@@ -1,63 +1,31 @@
 {
-More actions
-
   pkgs,
-
   lib,
-
   ...
-
 }: let
-
   deps = pkgs.stdenvNoCC.mkDerivation {
-
     name = "md2html-deps";
-
     dontUnpack = true;
-
     outputHashAlgo = "sha256";
-
     outputHashMode = "recursive";
-
-
     outputHash = "sha256-VrMoI+3lN+6KxHC5JdWpx45zCbqUowJYk+vwI4JD/S0=";
 
-
-
     buildPhase = ''
-
       ${lib.getExe pkgs.bun} add rehype-autolink-headings rehype-document rehype-format rehype-highlight rehype-raw rehype-slug rehype-stringify remark-definition-list remark-frontmatter remark-gfm remark-mdx remark-oembed remark-parse remark-rehype remark-wiki-link unified
-
       mv node_modules $out
-
     '';
-
   };
-
 in
-
   pkgs.stdenvNoCC.mkDerivation {
-
     name = "md2html";
-
     dontUnpack = true;
-
     src = ./.;
-
-    buildPhase = ''More actions
-
+    buildPhase = ''
       mkdir -p $out/{bin,share/md2html}
-
       cp -r $src/* $out/share/md2html
-
       cp -r ${deps} $out/share/md2html/node_modules
-
       chmod +x $out/share/md2html/md2html.js
-
       ln -s $out/share/md2html/md2html.js $out/bin/md2html
-
       substituteInPlace $out/bin/md2html --replace-fail "@@CSS@@" "${./md2html.css}"
-
     '';
-
   }
